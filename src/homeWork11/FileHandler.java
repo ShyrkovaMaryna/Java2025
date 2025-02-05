ffa954ab41df9264a8b8a52d638235257507ea3b
ShyrkovaMaryna <sirkovamarina7@gmail.com>
2025-02-06 00:21:10 +0200

    add home work 11

---
+++ src/homeWork11/FileHandler.java
@@ -0,0 +1,40 @@
+package homeWork11;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileHandler {
+
+    public String createFile(String strPath) {
+        Path newFile;
+        try {
+            newFile = Files.createFile(Path.of(strPath));
+        } catch (FileAlreadyExistsException e) {
+            return "File already exists!";
+        } catch (IOException e) {
+            return "Something wrong " + e.getMessage();
+        }
+        return "Created " + newFile;
+    }
+
+    public String writeToFile(Path path, String content) {
+        try {
+            Files.writeString(path, content);
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+        return "Recorded in " + path;
+    }
+
+    public String readFromFile(String path) {
+        try {
+            return Files.readString(
+                    Path.of(path)
+            );
+        } catch (IOException e) {
+            return "Something wrong " + e.getMessage();
+        }
+    }
+}
