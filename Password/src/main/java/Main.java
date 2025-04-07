51df64c1c4814004f019d3e2332ab38c428b1654
ShyrkovaMaryna <sirkovamarina7@gmail.com>
2025-04-07 14:07:46 +0300

    add home work 23

---
+++ Password/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.example.PasswordGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+        int passwordLength = 12;
+        String generatedPassword = PasswordGenerator.generatePassword(passwordLength);
+        System.out.println("Згенерований пароль: " + generatedPassword);
+    }
+}
