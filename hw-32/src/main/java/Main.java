a5a371a0ee9ee6c9f20990728fdf075ef8a3645a
ShyrkovaMaryna <sirkovamarina7@gmail.com>
2025-05-28 23:21:24 +0300

    home work 32

---
+++ hw-32/src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        CoffeeOrderBoard board = new CoffeeOrderBoard();
+
+        board.add("Alen");
+        board.add("Yoda");
+        board.add("Obi-van");
+        board.add("John Snow");
+
+        board.draw();
+
+        board.deliver();
+        board.deliver(33);
+
+        board.draw();
+    }
+}
